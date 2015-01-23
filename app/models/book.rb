ce2069d1c41e4816bc1c621577a9e0442dabc81e
charle692 <charle692@gmail.com>
2015-01-23 12:52:35 -0500

    Added header

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,12 @@
 class Book < ActiveRecord::Base
 	# Makes sure that both an isbn and quantity are entered
 	validates :isbn, :quantity, presence: true 
+	validate :isbn_length
+
+	private 
+	def isbn_length
+		unless isbn.size == 10 or isbn.size == 13
+			errors.add(:isbn, "numbers must be 10 or 13 characters long #{isbn}")
+		end
+	end
 end
