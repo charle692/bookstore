9b83a3c7705c27f7e10af2a045a7d6fabc0a0400
Ryan O'Connor <charle692@gmail.com>
2015-02-01 23:42:53 -0500

    Added seed and delete option in show book view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Book.new(isbn: "0151957479", quantity: "2").save
+Book.new(isbn: "0226103897", quantity: "2").save
+Book.new(isbn: "0226103900", quantity: "2").save
+Book.new(isbn: "0226104036", quantity: "2").save
+Book.new(isbn: "0226104044", quantity: "2").save
+Book.new(isbn: "0226770087", quantity: "2").save
+Book.new(isbn: "0345307675", quantity: "2").save
+Book.new(isbn: "0345341465", quantity: "2").save
+Book.new(isbn: "0439064872", quantity: "2").save
+Book.new(isbn: "0439136369", quantity: "2").save
+Book.new(isbn: "0439139600", quantity: "2").save
+Book.new(isbn: "0439358078", quantity: "2").save
+Book.new(isbn: "0439708184", quantity: "2").save
+Book.new(isbn: "0439785960", quantity: "2").save
+Book.new(isbn: "0440414806", quantity: "2").save
+Book.new(isbn: "0545128285", quantity: "2").save
+Book.new(isbn: "0547928203", quantity: "2").save
+Book.new(isbn: "0547928211", quantity: "2").save
+Book.new(isbn: "0618640150", quantity: "2").save
+Book.new(isbn: "0684835339", quantity: "2").save
+
+
