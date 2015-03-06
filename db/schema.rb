fd115067ec289786cb6ab01e9bbf9e246cfe4944
charle692 <charle692@gmail.com>
2015-03-06 12:47:30 -0500

    Added seends and changed summary type

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227222405) do
+ActiveRecord::Schema.define(version: 20150306174450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20150227222405) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "author"
-    t.string   "summary"
+    t.text     "summary"
     t.string   "publisher"
     t.string   "title"
   end
