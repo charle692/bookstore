b0abbd8ba21ad130c238b6c30a0990bb102585ea
charle692 <charle692@gmail.com>
2015-05-04 20:25:44 -0400

    Added title and removed unused home page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  
+  root 'books#index'
   get 'help'    => 'static_pages#help'
+  get 'checkout' => 'books#checkout'
+  get 'search' => 'books#search'
 
   # Creates a REST resource for books
   resources :books
-  get 'search' => 'books#search'
-  root 'books#index'
-  get 'checkout' => 'books#checkout'
 end
